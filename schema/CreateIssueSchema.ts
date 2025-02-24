bb307f73d09e62e27aab866e98ff13f9fcff4259
Travis <Travisang40@gmail.com>
2025-02-24 16:37:22 +0800

    increased title word limit

--- schema/CreateIssueSchema.ts
+++ schema/CreateIssueSchema.ts
@@ -1,7 +1,7 @@
 import z from "zod"
 
 export const CreateIssueSchema = z.object({ 
-    title : z.string().min(5,{message : "Username must be at least 5 characters"}).max(30,{message : "Username must not be above 30 characters"}),
+    title : z.string().min(5,{message : "Title must be at least 5 characters"}).max(80,{message : "Title must not be above 30 characters"}),
     content : z.any()
 
 })
